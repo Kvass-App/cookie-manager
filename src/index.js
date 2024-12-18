79ea0d6722dd896bcc49e55bffcc9ae922db0e57
Lisbeth Berg <lisbeth@elder.no>
2024-12-18 12:33:27 +0100

    perf(index): description on items is removed

--- src/index.js
+++ src/index.js
@@ -1,58 +1,52 @@
 function CookieManager(options = {}) {
-  let { onChange = () => {}, container = document.body } = options
+  let { onChange = () => { }, container = document.body } = options
 
-  let value
+  let value = []
   let view = 'Default'
   let el = document.createElement('div')
   el.classList.add('cookie-manager')
 
+  function hasChanges() {
+    return value.filter(
+      (i) => i !== (options.consents.find((c) => c.required) || {}).id
+    ).length
+  }
   const Renderer = {
-    getConfigView: options => `
-  <div class="cookie-manager__modal">
-    ${options.consents
-      .map(
-        c => `
-      <label class="cookie-manager__consent">
-        <input type="checkbox" name="consent" value="${c.id}" ${
-          (value && value.includes(c.id)) || (!value && (c.default || c.required)) ? 'checked' : ''
-        } ${c.required ? 'disabled="disabled"' : ''} aria-describedby="cookie-manager-describedby-${
-          c.id
-        }" />
-        <span class="cookie-manager__consent-content">
-          <h3 class="cookie-manager__consent-title">${c.title}</h3>
-          <p class="cookie-manager__consent-description" id="cookie-manager-describedby-${c.id}">${
-          c.description
-        }</p>
-        </span>
-      </label>
-    `,
-      )
-      .join('')}
-      <div class="cookie-manager__actions">
-      <button type="button" class="cookie-manager__accept">${options.labels.acceptAll}</button>
-        <button type="button" class="cookie-manager__confirm cookie-manager__button-primary">${
-          options.labels.confirm
-        }</button>
-      </div>
-  </div>
-`,
-    getDefaultView: options => `
+    getDefaultView: (options) => `
     <div class="cookie-manager__modal">
-      <h2 class="cookie-manager__title">${options.title}</h2>
+      <h3 class="cookie-manager__title">${options.title}</h3>
       <p class="cookie-manager__description">${options.description}</p>
-      
-      <div class="cookie-manager__actions">
-        <button type="button" class="cookie-manager__configure">${options.labels.configure}</button>
-        <button type="button" class="cookie-manager__accept cookie-manager__button-primary">${options.labels.accept}</button>
-      </div>
+    
+      ${options.consents
+        .map(
+          (c) => `
+        <label class="cookie-manager__consent">
+
+          <input type="checkbox" name="consent" value="${c.id}" ${value && value.includes(c.id) ? 'checked' : ''
+            } ${c.required ? 'disabled="disabled"' : ''} aria-describedby="cookie-manager-describedby-${c.id
+            }" />
+          <span class="cookie-manager__consent-content">
+            <span class="cookie-manager__consent-title">${c.title}</span>
+          </span>
+        </label>
+      `
+        )
+        .join('')}
+        <div class="cookie-manager__actions">
+        <button type="button" class="cookie-manager__decline">${options.labels.declineAll}</button>
+           ${hasChanges()
+        ? `<button type="button" class="cookie-manager__confirm cookie-manager__button-primary">${options.labels.confirm
+        }</button>`
+        : `<button type="button" class="cookie-manager__accept cookie-manager__button-primary">${options.labels.acceptAll
+        }</button>`
+      }
+        </div>
     </div>
 `,
     getStyle: () => `
   .cookie-manager {
     --cookie-manager-spacing: 2rem;
-
     background-color: rgba(0,0,0,0.8);
-
     position: fixed;
     top: 0;
     left: 0;
@@ -60,11 +54,9 @@ function CookieManager(options = {}) {
     height: 100%;
     padding: var(--cookie-manager-spacing);
     z-index: 10000;
-
     display: flex;
     justify-content: center;
     align-items: flex-end;
-
     accent-color: var(--primary);
   }
 
@@ -72,21 +64,20 @@ function CookieManager(options = {}) {
     background-color: white;
     padding: var(--cookie-manager-spacing);
     border-radius: var(--border-radius, 3px);
-    max-width: 700px;
+    max-width: 500px;
     max-height: 100dvh;
     overflow-y: auto;
   }
 
+
   .cookie-manager__title,
   .cookie-manager__consent-title {
-    font-weight: bold;
-    font-size: 1.25rem;
-    margin-bottom: 1rem;
     line-height: 1;
   }
 
   .cookie-manager__description {
     white-space: pre-line;
+    font-size: 0.8em;
   }
 
   .cookie-manager__consent {
@@ -118,16 +109,10 @@ function CookieManager(options = {}) {
     transform: scale(1.2);
   }
 
-
   .cookie-manager__consent-title {
-    font-size: 1rem;
+    font-size: 0.9rem;
   }
   
-  .cookie-manager__consent-description {
-    font-size: .8rem;
-    white-space: pre-line;
-  }
-
   .cookie-manager__actions {
     display: flex;
     justify-content: end;
@@ -149,11 +134,13 @@ function CookieManager(options = {}) {
     border-color: var(--primary, #1d56d8);
     color: var(--primary-contrast, white) !important;
   }
+
+
   
 
   @media (max-width: 900px) {
     .cookie-manager {
-      --cookie-manager-spacing: 1rem;
+      --cookie-manager-spacing: 1.5rem;
       padding: 0;
     }
   }
@@ -171,51 +158,74 @@ function CookieManager(options = {}) {
 
   document.head.appendChild(style)
 
-  let save = consents => {
+  let save = (consents) => {
     window.localStorage.setItem(`cookie-manager`, consents)
     value = consents
   }
   let load = () => {
     let qs = new URLSearchParams(location.search)
-    let raw = qs.get('cookie-manager') || window.localStorage.getItem('cookie-manager')
+    let raw =
+      qs.get('cookie-manager') || window.localStorage.getItem('cookie-manager')
     if (!raw) return
     value = raw.split(',')
   }
-  let updateOptions = newOptions => {
+  let updateOptions = (newOptions) => {
     options = newOptions
   }
   let hide = () => container.removeChild(el)
   let render = () => {
+    if (!value.length) {
+      value = options.consents
+        .filter((i) => i.required || i.default)
+        .map((i) => i.id)
+    }
     el.innerHTML = Renderer[`get${view}View`](options)
+
     if (!container.contains(el)) container.appendChild(el)
   }
   let init = () => {
     load()
     el.addEventListener('click', handlers.submit)
-    if (!value) render()
+    el.addEventListener('click', handlers.update)
+    if (!value.length) render()
     else onChange(value)
   }
   let destroy = () => {
     el.removeEventListener('click', handlers.submit)
+    el.removeEventListener('click', handlers.update)
     hide()
   }
 
   let handlers = {
-    submit: e => {
-      if (!['confirm', 'accept', 'configure'].some(i => e.target.className.includes(i))) return
+    update: (e) => {
+      if (e.target.name !== 'consent') return
+      if (e.target.checked) {
+        value.push(e.target.value)
+      } else if (value.includes(e.target.value)) {
+        const index = value.indexOf(e.target.value)
+        value.splice(index, 1)
+      }
+      render()
+    },
+    submit: (e) => {
+      if (
+        !['confirm', 'accept', 'decline'].some((i) =>
+          e.target.className.includes(i)
+        )
+      )
+        return
 
-      let inputs = Array.from(el.querySelectorAll('input'))
       let consents = []
 
-      if (e.target.className.includes('configure')) {
-        view = 'Config'
-        return render()
+      if (e.target.className.includes('accept'))
+        consents.push(...options.consents.map((c) => c.id))
+      if (e.target.className.includes('confirm')) consents = value
+      if (e.target.className.includes('decline')) {
+        //make sure to always include required
+        consents = value.filter(
+          (i) => i === (options.consents.find((c) => c.required) || {}).id
+        )
       }
-
-      if (e.target.className.includes('accept')) consents.push(...options.consents.map(c => c.id))
-      if (e.target.className.includes('confirm'))
-        consents.push(...inputs.filter(e => e.checked).map(e => e.value))
-
       onChange(consents)
       save(consents)
       hide()
@@ -223,7 +233,6 @@ function CookieManager(options = {}) {
   }
 
   init()
-
   return {
     updateOptions,
     show: () => {
